6ffc420de0d49eb85d75ac0c04654043663ae253
eb1 <ebrommers@gmail.com>
2017-11-03 11:52:37 -0500

    initial checkin

---
+++ tests/tests.js
@@ -0,0 +1,141 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+/* jshint jasmine: true */
+
+exports.defineAutoTests = function () {
+    "use strict";
+    
+    describe('Env (navigator.Env)', function () {
+        
+        it("should exist", function () {
+            expect(navigator.Env).toBeDefined();
+        });
+        
+        it("should contain a getExternalStorageState function", function () {
+            expect(typeof navigator.Env.getExternalStorageState).toBeDefined();
+            expect(typeof navigator.Env.getExternalStorageState).toBe("function");
+        });
+
+        it("should contain a isExternalStorageEmulated function", function () {
+            expect(typeof navigator.Env.isExternalStorageEmulated).toBeDefined();
+            expect(typeof navigator.Env.isExternalStorageEmulated).toBe("function");
+        });
+
+        it("should contain a isExternalStorageRemovable function", function () {
+            expect(typeof navigator.Env.isExternalStorageRemovable).toBeDefined();
+            expect(typeof navigator.Env.isExternalStorageRemovable).toBe("function");
+        });
+        
+        it("should contain a getDirectory function", function () {
+            expect(typeof navigator.Env.getDirectory).toBeDefined();
+            expect(typeof navigator.Env.getDirectory).toBe("function");
+        });
+    });
+};
+    
+exports.defineManualTests = function (contentEl, createActionButton) {
+    "use strict";
+    
+    var logMessage = function (message, color) {
+        var log = document.getElementById('info'),
+            logLine = document.createElement('div');
+
+        if (color) {
+            logLine.style.color = color;
+        }
+        logLine.innerHTML = message;
+        log.appendChild(logLine);
+    },
+
+        clearLog = function () {
+            var log = document.getElementById('info');
+            log.innerHTML = '';
+        },
+
+        getExternalStorageStateTest = function () {
+            clearLog();
+            console.log("getExternalStorageStateTest()");
+            if (navigator.Env) {
+                navigator.Env.getExternalStorageState(
+                    function (result) {
+                        console.log("getExternalStorageStateTest() - value returned" + result);
+                    },
+                    function (error) {
+                        logMessage(error);
+                    }
+                );
+            } else {
+                console.log("Plugin error: Environment plugin not found (is it installed?)");
+            }
+        },
+
+        isExternalStorageEmulatedTest = function () {
+            clearLog();
+            console.log("isExternalStorageEmulatedTest()");
+            if (navigator.Env) {
+                navigator.Env.isExternalStorageEmulated(
+                    function (result) {
+                        console.log("isExternalStorageEmulatedTest() - value returned" + result);
+                    },
+                    function (error) {
+                        logMessage(error);
+                    }
+                );
+            } else {
+                console.log("Plugin error: Environment plugin not found (is it installed?)");
+            }
+        },
+
+        isExternalStorageRemovableTest = function () {
+            clearLog();
+            console.log("isExternalStorageRemovableTest()");
+            if (navigator.Env) {
+                navigator.Env.isExternalStorageRemovable(
+                    function (result) {
+                        console.log("isExternalStorageRemovableTest() - value returned" + result);
+                    },
+                    function (error) {
+                        logMessage(error);
+                    }
+                );
+            } else {
+                console.log("Plugin error: Environment plugin not found (is it installed?)");
+            }
+        },
+        
+        device_tests = '<p/><div id="cdv_storage_state"></div><p/><div id="cdv_storage_emulated"></div><p/><div id="cdv_storage_removable"></div><p/><div id="cdv_directories"></div>';
+    
+    contentEl.innerHTML = '<div id="info"></div>' + device_tests;
+
+    createActionButton('getExternalStorageState', function () {
+        getExternalStorageStateTest();
+    }, "cdv_storage_state");
+
+    createActionButton('isExternalStorageEmulated', function () {
+        isExternalStorageEmulatedTest();
+    }, "cdv_storage_emulated");
+
+    createActionButton('isExternalStorageRemovable', function () {
+        isExternalStorageRemovableTest();
+    }, "cdv_storage_removable");
+
+};
